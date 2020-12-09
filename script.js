f928326a6362fc78888ce361f028eeef9c2bfe22
Oleh <53017896+Namhkok@users.noreply.github.com>
2020-12-09 23:59:28 +0200

    res

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-
-function createRespondent() {
-    let number; // кількість членів сімї
-    let age ;
-    let sex ['male', 'famele'];
-    let famyli;
-    let; // одружен розведені тощо ;
-    let edu; 
-    let ocupation; // sity.town.village
-    let region;
-
-}
+click.onclick = function countRabbits() {
+    var gender = ['Чоловік', 'Жінка'];
+    var rand = Math.floor(Math.random() * gender.length);  
+    var idr = document.getElementById('gender');
+    idr.innerHTML = ('Стать: ' +gender[rand]);
+  
+    var place = ['Київ', 'Львів', 'Харків', 'Одеса', 'Тернопіль', 'Івано-Франківськ'];
+    var rand = Math.floor(Math.random() * place.length);
+    var idre = document.getElementById('place');
+    idre.innerHTML = ('Місто: ' + place[rand]);
+  
+    var education = ['Вища', 'Професійна', 'Середня'];
+    var rand = Math.floor(Math.random() * education.length);
+    var idre = document.getElementById('education');
+    idre.innerHTML = ('Освіта: ' + education[rand]);
+    }
