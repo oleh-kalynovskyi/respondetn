f6b3a49005b0e7fb7431516c4382994bb414ec51
Oleh <53017896+Namhkok@users.noreply.github.com>
2020-12-10 20:29:45 +0200

    res

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-click.onclick = function countRabbits() {
+click.onclick = function() {
     var gender = ['Чоловік', 'Жінка'];
     var rand = Math.floor(Math.random() * gender.length);  
     var idr = document.getElementById('gender');
-    idr.innerHTML = ('Стать: ' +gender[rand]);
+    idr.innerHTML = ('Стать: ' + gender[rand]);
   
     var place = ['Київ', 'Львів', 'Харків', 'Одеса', 'Тернопіль', 'Івано-Франківськ'];
     var rand = Math.floor(Math.random() * place.length);
-    var idre = document.getElementById('place');
-    idre.innerHTML = ('Місто: ' + place[rand]);
+    var idr = document.getElementById('place');
+    idr.innerHTML = ('Місто: ' + place[rand]);
+
+    var family = ['Розлучений/на', 'Одружений/на', 'вдова/вдівець'];
+    var rand = Math.floor(Math.random() * family.length);  
+    var idr = document.getElementById('family');
+    idr.innerHTML = ('Сімейний стан: ' + family[rand]);
   
     var education = ['Вища', 'Професійна', 'Середня'];
     var rand = Math.floor(Math.random() * education.length);
-    var idre = document.getElementById('education');
-    idre.innerHTML = ('Освіта: ' + education[rand]);
+    var idr = document.getElementById('education');
+    idr.innerHTML = ('Освіта: ' + education[rand]);
+        
+    function randomInteger(min, max) {
+        // let rand = min - 0 + Math.random() * (max - min + 0);
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+        }
+        var idr = document.getElementById('ege');
+        idr.innerHTML = ( 'Вік: ' + randomInteger(23, 80) );
+    
+    var back = ['Донецьк', 'Луганськ'];
+    var rand = Math.floor(Math.random() * back.length);
+    var idr = document.getElementById('back');
+    idr.innerHTML = ('Попереднє місце: ' + back[rand]);
+        
     }
